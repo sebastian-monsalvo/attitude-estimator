e8f9738589e6e244f389fd403e31237d87c84ffc
Sebastian <monsalvo@stanford.edu>
2026-09-02 18:43:16 -0400

    finally saw the biasgit add . 2 bugs: I was not dividing dt by 1000 to convert ms to s and i wasn't passing total_roll by reference to the function roll so every iteration of void loop() would pass total_roll as 0 making it always come back to 0

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -8,20 +8,22 @@
 #include <utility/imumaths.h>
 #include <Teleplot.h>
 
-unsigned long roll(unsigned long startMillis, unsigned long currentMillis, Adafruit_BNO055 bno, float total_roll, Teleplot tele) {
+unsigned long roll(unsigned long startMillis, unsigned long currentMillis, Adafruit_BNO055 bno, float& total_roll, Teleplot tele) {
 
     float dt = (float) currentMillis - startMillis;
-    // Serial.print("dt: ");
-    // Serial.print(dt);
-    // Serial.print("   ");
+
     imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
     float total_roll_prev = total_roll;
-    total_roll = total_roll + dt * gyro.x();
-    if (abs(total_roll - total_roll_prev) < 1000) {
-        total_roll = total_roll;
-    } else {
+    total_roll = total_roll + (dt / 1000.0) * gyro.x(); // dt in ms
+    
+    if (abs(total_roll - total_roll_prev) < 0.5) { // low pass filter, if innovation too small: dont count it
         total_roll = total_roll_prev;
+    } else {
+        total_roll = total_roll;
     }
+    Serial.print(">raw_gyro_x:");
+    Serial.println(gyro.x());
+
     Serial.print(">total_roll:");
     Serial.println(total_roll);
 
--- src/helpers.h
+++ src/helpers.h
@@ -10,4 +10,5 @@
 
 std::vector<float> f(std::vector<float> angles);
 
-unsigned long roll(unsigned long startMillis, unsigned long currentMillis, Adafruit_BNO055 bno, float total_roll, Teleplot tele);
+// big bug was you were not passing by reference so in void loop() total_roll would reset to 0 in every iteration lol
+unsigned long roll(unsigned long startMillis, unsigned long currentMillis, Adafruit_BNO055 bno, float& total_roll, Teleplot tele);
